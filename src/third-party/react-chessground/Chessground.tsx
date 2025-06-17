2750d5cbacc667d4126706f0b8c5bbd35a8a0cb8
MikeD <isofarro2@gmail.com>
2025-06-17 08:32:27 +0100

    Copy in my Chessground wrapper.

---
+++ src/third-party/react-chessground/Chessground.tsx
@@ -0,0 +1,46 @@
+import { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
+
+import { Chessground as ChessgroundApi } from 'chessground';
+import type { Config } from 'chessground/config';
+import type { Api } from 'chessground/api';
+import type { Key } from 'chessground/types';
+
+interface Props {
+  width?: number;
+  height?: number;
+  contained?: boolean;
+  config?: Config;
+}
+
+const Chessground = forwardRef<Api | undefined, Props>(
+  ({ width = 900, height = 900, config = {}, contained = false }: Props, apiRef) => {
+    const [api, setApi] = useState<Api | undefined>();
+    const divRef = useRef<HTMLDivElement>(null);
+
+    useImperativeHandle(apiRef, () => {
+      return api;
+    }, [api]);
+
+    useEffect(() => {
+      if (divRef.current && !api) {
+        const chessgroundApi = ChessgroundApi(divRef.current, config);
+        setApi(chessgroundApi);
+      }
+    }, [divRef.current, api]);
+
+    useEffect(() => {
+      if (api) {
+        api.set(config);
+      }
+    }, [config]);
+
+    return (
+      <div style={{ height: contained ? '100%' : height, width: contained ? '100%' : width }}>
+        <div ref={divRef} style={{ height: '100%', width: '100%', display: 'table' }} />
+      </div>
+    );
+  }
+);
+
+export type { Api, Config, Key };
+export default Chessground;
