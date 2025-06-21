1c106c7b5a1f1f625090db11212f80f7b44f1cf4
MikeD <isofarro2@gmail.com>
2025-06-21 18:04:28 +0100

    format

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { ExplorerPane } from './features/opening-trees/components/ExplorerPane';
 import { START_POSITION_FEN } from './core/constants';
 const FEN_CLOSED_RUY_LOPEZ = 'r1bq1rk1/2p1bppp/p1np1n2/1p2p3/4P3/1BP2N1P/PP1P1PP1/RNBQR1K1 b - -';
 
-
 export const App = () => {
   return (
     <div className="App">
