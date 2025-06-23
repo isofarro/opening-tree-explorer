5e0edead144388b971eb698b10da1206a17932bb
MikeD <isofarro2@gmail.com>
2025-06-23 21:07:36 +0100

    remove unused import.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import 'chessground/assets/chessground.brown.css';
 import 'chessground/assets/chessground.cburnett.css';
 
 import { ExplorerPane } from './features/opening-trees/components/ExplorerPane';
-import { START_POSITION_FEN } from './core/constants';
+// import { START_POSITION_FEN } from './core/constants';
 const FEN_CLOSED_RUY_LOPEZ = 'r1bq1rk1/2p1bppp/p1np1n2/1p2p3/4P3/1BP2N1P/PP1P1PP1/RNBQR1K1 b - -';
 
 export const App = () => {
