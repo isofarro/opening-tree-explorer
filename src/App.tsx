b774fe2113b690b3ef9bad56c8633a88ee5e09d9
MikeD <isofarro2@gmail.com>
2025-07-07 12:25:18 +0100

    Add DEFAULT_TREE

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,13 @@ import { ExplorerPane } from './features/opening-trees/components/ExplorerPane';
 import { OpeningTreeProvider } from './features/opening-trees/providers/OpeningTreeProvider';
 import { START_POSITION_FEN } from './core/constants';
 const FEN_CLOSED_RUY_LOPEZ = 'r1bq1rk1/2p1bppp/p1np1n2/1p2p3/4P3/1BP2N1P/PP1P1PP1/RNBQR1K1 b - -';
+const DEFAULT_TREE = 'iccf';
 
 export const App = () => {
   return (
     <div className="App">
       <OpeningTreeProvider>
-        <ExplorerPane tree="twic-2025" position={START_POSITION_FEN} />
+        <ExplorerPane tree={DEFAULT_TREE} position={START_POSITION_FEN} />
       </OpeningTreeProvider>
     </div>
   );
