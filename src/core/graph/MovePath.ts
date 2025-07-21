012fc89735b80db8479afe757d4e65bff11d2017
MikeD <isofarro2@gmail.com>
2025-07-21 19:33:00 +0100

    Class to track the path through the ChessGraph.

---
+++ src/core/graph/MovePath.ts
@@ -0,0 +1,41 @@
+import { START_POSITION_FEN } from '../constants';
+import type { FenString } from '../types';
+import type { Move } from './types';
+
+export class MovePath {
+  startFen: FenString;
+  firstMoveIsBlack: boolean;
+  path: Move[];
+  moveIdx: number;
+
+  constructor(startFen: FenString = START_POSITION_FEN) {
+    this.startFen = startFen;
+    this.firstMoveIsBlack = startFen.includes(' b ');
+
+    this.path = [];
+    this.moveIdx = 0;
+  }
+
+  addMove(fromFen: FenString, move: Move) {
+    console.log('[PATH] addMove:', fromFen, move, this.path);
+
+    // Find the move with a toFen that matches the fromFen
+    if (this.path.length === 0 || this.path[this.path.length - 1].toFen === fromFen) {
+      // The main usecase is it's the last move
+      this.path.push(move);
+      return;
+    }
+
+    // Search the movePath for the move
+    const moveIdx = this.path.findIndex((move) => move.toFen === fromFen);
+    if (moveIdx > -1) {
+      // Remove moves after moveIdx and add in the new move
+      this.path.splice(moveIdx + 1);
+      this.path.push(move);
+      return;
+    }
+
+    // TODO: use the graph to find the move, and then backtrack
+    throw new Error(`Move not found in movePath`);
+  }
+}
