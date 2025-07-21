2bebc7e47317139a27fc9806c37c6fd924eb4659
MikeD <isofarro2@gmail.com>
2025-07-21 19:34:38 +0100

    Compose ChessGraph and MovePath with a single class.

---
+++ src/core/graph/ChessMoveGraph.ts
@@ -0,0 +1,29 @@
+import { START_POSITION_FEN } from '../constants';
+import type { FenString } from '../types';
+import { ChessGraph } from './ChessGraph';
+import type { IChessGraph } from './iChessGraph';
+import { MovePath } from './MovePath';
+import type { Move } from './types';
+
+export class ChessMoveGraph implements IChessGraph {
+  graph: ChessGraph; // The graph of positions and moves
+  moves: MovePath; // The path through the graph
+
+  constructor(startFen: FenString = START_POSITION_FEN) {
+    this.graph = new ChessGraph();
+    this.moves = new MovePath(startFen);
+  }
+
+  addMove(fromFen: FenString, move: Move) {
+    this.graph.addMove(fromFen, move);
+    this.moves.addMove(fromFen, move);
+  }
+
+  findPosition(fen: FenString) {
+    return this.graph.findPosition(fen);
+  }
+
+  getMovePath(): Move[] {
+    return this.moves.path;
+  }
+}
