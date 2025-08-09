18579406afd809161bc35e2bc3ebf0b27b4bc7ee
MikeD <isofarro2@gmail.com>
2025-08-09 18:43:12 +0100

    default to twic-2015 tree

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,14 @@ import 'chessground/assets/chessground.cburnett.css';
 
 import { ExplorerPane } from '~/features/explorer/components/ExplorerPane';
 import { OpeningTreeProvider } from '~/features/opening-trees/providers/OpeningTreeProvider';
-import { START_POSITION_FEN } from '~/core/constants';
-const FEN_CLOSED_RUY_LOPEZ = 'r1bq1rk1/2p1bppp/p1np1n2/1p2p3/4P3/1BP2N1P/PP1P1PP1/RNBQR1K1 b - -';
-const DEFAULT_TREE = 'iccf';
+import { DEFAULT_OPENING_TREE_NAME, START_POSITION_FEN } from '~/core/constants';
+// const FEN_CLOSED_RUY_LOPEZ = 'r1bq1rk1/2p1bppp/p1np1n2/1p2p3/4P3/1BP2N1P/PP1P1PP1/RNBQR1K1 b - -';
 
 export const App = () => {
   return (
     <div className="App">
       <OpeningTreeProvider>
-        <ExplorerPane tree={DEFAULT_TREE} position={START_POSITION_FEN} />
+        <ExplorerPane tree={DEFAULT_OPENING_TREE_NAME} position={START_POSITION_FEN} />
       </OpeningTreeProvider>
     </div>
   );
