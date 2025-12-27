f1199333e70c931989686b234b7f6fb0b197b9ce
MikeD <isofarro2@gmail.com>
2025-12-27 21:59:36 +0000

    integrate cloud analysis hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 
 import 'chessground/assets/chessground.base.css';
@@ -9,12 +10,16 @@ import { OpeningTreeProvider } from '~/features/opening-trees/providers/OpeningT
 import { DEFAULT_OPENING_TREE_NAME, START_POSITION_FEN } from '~/core/constants';
 // const FEN_CLOSED_RUY_LOPEZ = 'r1bq1rk1/2p1bppp/p1np1n2/1p2p3/4P3/1BP2N1P/PP1P1PP1/RNBQR1K1 b - -';
 
+const queryClient = new QueryClient();
+
 export const App = () => {
   return (
     <div className="App">
-      <OpeningTreeProvider>
-        <ExplorerPane tree={DEFAULT_OPENING_TREE_NAME} position={START_POSITION_FEN} />
-      </OpeningTreeProvider>
+      <QueryClientProvider client={queryClient}>
+        <OpeningTreeProvider>
+          <ExplorerPane tree={DEFAULT_OPENING_TREE_NAME} position={START_POSITION_FEN} />
+        </OpeningTreeProvider>
+      </QueryClientProvider>
     </div>
   );
 };
